28c290357d18c4aa837e5f8b8f30440657ea4bb7
Bineeta17 <bineeta.joshi@noduco.co.in>
2020-05-11 15:19:10 +0530

    course class

--- src/main/java/com/campus/Campus/Model/Course.java
+++ src/main/java/com/campus/Campus/Model/Course.java
@@ -1,11 +1,11 @@
 package com.campus.Campus.Model;	
 
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
+
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -21,6 +21,9 @@ public class Course {
 	@ApiModelProperty(notes="Name")
 	String c_name;
 
+	@OneToOne(mappedBy="course")
+	private Student stu;
+	
 	public int getC_id() {
 		return c_id;
 	}
